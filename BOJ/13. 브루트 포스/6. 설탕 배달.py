52d3ab1cd674220fa55121031f468d35cc6d312f
heylosa <tjdwns2729@gmail.com>
2023-05-23 21:00:13 +0900

    Algorithm

--- BOJ/13. 브루트 포스/6. 설탕 배달.py
+++ BOJ/13. 브루트 포스/6. 설탕 배달.py
@@ -1,2 +1,14 @@
 # 2839번
 # 한때는 이 문제가 "기본 수학 1" 단계에 있었지만, 사실 브루트 포스로 푸는 게 더 쉽습니다.
+N = int(input())
+k = N // 5
+
+
+if k == 0:
+    print(-1)
+    break
+else:
+    for i in range(k + 1):
+        if (N - 5 * (k - i)) % 3 == 0:
+            print((k - i) + ((N - 5 * (k - i)) // 3))
+            break

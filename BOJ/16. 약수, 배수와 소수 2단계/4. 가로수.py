9039bb725bbf24e8ad10a5569589356e974d8cdc
heylosa <tjdwns2729@gmail.com>
2023-06-04 12:05:38 +0900

    Algorithm

---
+++ BOJ/16. 약수, 배수와 소수 2단계/4. 가로수.py
@@ -0,0 +1,20 @@
+import math
+import sys
+from math import gcd
+N = int(input())
+a = int(input())
+A = []
+for i in range (N-1):
+    number = int(sys.stdin.readline())
+    A.append(number-a)
+    a = number
+
+gcd = A[0]
+for j in range (1, len(A)):
+    gcd = math.gcd(gcd,A[j])
+
+R = 0
+for w in A:
+    R += w // gcd - 1
+
+print(R)

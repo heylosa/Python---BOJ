7076cd63235f26d6d391b5174d06ca1e1d762118
heylosa <tjdwns2729@gmail.com>
2023-06-14 01:04:57 +0900

    Algorithm

--- BOJ/11-20/19. 스택/2. 제로.py
+++ BOJ/11-20/19. 스택/2. 제로.py
@@ -4,6 +4,13 @@
 import sys
 
 K = int(input())
+stack = []
 for _ in range (K):
-    A = int(sys.stdin.readline())
+    A = int(sys.stdin.readline().rstrip())
+    if A == 0:
+        stack.pop()
+    else:
+        stack.append(A)
+
+print(sum(stack))
 
--- BOJ/11-20/19. 스택/3. 괄호.py
+++ BOJ/11-20/19. 스택/3. 괄호.py
@@ -1,2 +1,14 @@
 # 9012
 # 주어진 문자열이 올바른 괄호열인지 판단하는 문제
+import sys
+T = int(input())
+for _ in range (T):
+    str = sys.stdin.readline().rstrip()
+    for _ in range (int(len(str)/2)):
+        str.replace('()','')
+        print(str)
+    if str == '':
+        print("YES")
+    else:
+        print("NO")
+

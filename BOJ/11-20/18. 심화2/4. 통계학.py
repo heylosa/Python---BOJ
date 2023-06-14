ffe18dd105e0b2309d1c75f5e10a03c2d4f14b4f
heylosa <tjdwns2729@gmail.com>
2023-06-14 14:19:49 +0900

    Algorithm

--- BOJ/11-20/18. 심화2/4. 통계학.py
+++ BOJ/11-20/18. 심화2/4. 통계학.py
@@ -3,11 +3,33 @@ import sys; input = sys.stdin.readline
 import statistics
 
 lst = []
-for _ in range (int(input().strip())):
-    lst.append(int(input().strip()))
+for _ in range (int(input().rstrip())):
+    lst.append(int(input().rstrip()))
 
 
 print(round(statistics.mean(lst)))
-print(round(statistics.median(lst)))
-print(round(statistics.mode(lst)))
-print(round(max(lst)-min(lst)))
+print(statistics.median(lst))
+
+dic={}
+for i in lst:
+    if i in dic:
+        dic[i] += 1
+    else:
+        dic[i] = 0
+
+maximum = max(dic.values())
+max_lst = []
+
+for i in dic:
+    if maximum == dic[i]:
+        max_lst.append(i)
+
+max_lst.sort()
+
+if len(max_lst) > 1:
+    print(max_lst[1])
+else:
+    print(max_lst[0])
+
+
+print(max(lst)-min(lst))
